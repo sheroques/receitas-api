101ad72f63b9a728d2978680e9b3cb3936f66df8
LAB34 <lab34@fatec.com>
2024-09-19 20:27:11 -0300

    exercicio 1

---
+++ receitas-api/src/test/java/com/example/receitas_api/ReceitasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receitas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
